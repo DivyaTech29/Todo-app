cfa2e9bbee894ee9a8e1cf4c00cce95b2f6ae0c3
DivyaTech29 <nadavapallisaidivya@gmail.com>
2024-08-26 17:21:10 +0530

    chnage port 4000 to 8080

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use('/tasks', tasksRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
